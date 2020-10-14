1074b4425fb6e1297693675b7f2e49696a87ed45
Pavel <mikhajlin2010@yandex.ru>
2020-10-14 21:08:24 +0300

    Init commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Element{
+public:
+private:
+    string poligon_centaer;
+    string poligon_abris;
+    string type;
+};
+
+class Obj{
+public:
+    vector<Element>array{} ;
+private:
+};
+
+int main()
+{
+
+}
